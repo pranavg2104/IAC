00b2449d29e63c9dae38fa34a7eb6ec5df3e4521
Pranav Govekar <pranavg2104@gmail.com>
2026-05-23 22:00:04 +0530

    Updating the configurations

--- Terraform/2-tier-application/terra-configs/data.tf
+++ Terraform/2-tier-application/terra-configs/data.tf
@@ -4,19 +4,22 @@ data "aws_availability_zones" "available" {
 
 data "aws_ami" "ubuntu" {
   most_recent = true
-  owners = ["amazon"]
+  owners      = ["amazon"]
   filter {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
   }
 }
 
+data "aws_secretsmanager_secret_version" "rds_credentials" {
+  secret_id = "employee-mgnt/rds-credentials"  
+}
+
 locals {
-  db_creds = jsondecode(aws_secretsmanager_secret_version.admin.secret_string)
   userdata = templatefile("${path.module}/userdata.tpl", {
     DB_NAME = "employee_db"
-    DB_USER = local.db_creds.username
-    DB_PASSWORD = local.db_creds.password
+    DB_USER = jsondecode(data.aws_secretsmanager_secret_version.rds_credentials.secret_string)["username"]
+    DB_PASSWORD = jsondecode(data.aws_secretsmanager_secret_version.rds_credentials.secret_string)["password"]
     DB_PORT = "3306"
     DB_HOST = module.db.db_instance_endpoint
   })
--- Terraform/2-tier-application/terra-configs/rds.tf
+++ Terraform/2-tier-application/terra-configs/rds.tf
@@ -8,10 +8,10 @@ module "db"{
   engine_version = "8.0"
   major_engine_version = "8.0"
   family = "mysql8.0"
-  instance_class = "t3.micro"
-  username = local.db_creds.username
+  instance_class = "db.t3.micro"
+  username = jsondecode(data.aws_secretsmanager_secret_version.rds_credentials.secret_string)["username"]
   manage_master_user_password = true
-  password_wo = local.db_creds.password
+  password_wo = jsondecode(data.aws_secretsmanager_secret_version.rds_credentials.secret_string)["password"]
   vpc_security_group_ids = [module.rds_security_group.security_group_id]
   multi_az = true
   skip_final_snapshot = true
